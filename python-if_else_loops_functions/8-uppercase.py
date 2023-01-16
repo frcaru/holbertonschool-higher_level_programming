a5a21f6ce2efade9b48cbb8cbba84dd584a633d4
Marine <5641@holbertonstudents.com>
2023-01-16 19:21:34 +0100

    Task 8: 1st try

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    strupper = ""
+    for i in range(len(str)):
+        if str[i] >= 'a' and str[i] <= 'z':
+            strupper += chr(ord(str[i]) - 32)
+        else:
+            strupper += str[i]
+    print(strupper)
