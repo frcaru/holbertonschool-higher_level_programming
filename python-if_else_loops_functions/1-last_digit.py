552e57c3c1f50c851348ccff7ad238d05a220723
Marine <5641@holbertonstudents.com>
2023-01-16 17:27:38 +0100

    Task 1: pep8 correction

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,11 @@ last_digit = abs(number) % 10
 """ value_if_true if condition else value_if_false """
 last_digit = last_digit * -1 if number < 0 else last_digit
 
+print(f"Last digit of {number} is {last_digit}", end =" ")
+
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print("and is greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+    print("and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print("and is less than 6 and not 0")
