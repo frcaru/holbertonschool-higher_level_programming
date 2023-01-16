606bc040cb8b48948492ec3dea0796d100153199
Marine <5641@holbertonstudents.com>
2023-01-16 17:24:51 +0100

    Task 1: negative number correction

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+
+last_digit = abs(number) % 10
+""" value_if_true if condition else value_if_false """
+last_digit = last_digit * -1 if number < 0 else last_digit
+
 if last_digit > 5:
-    if number < 0:
-        print(f"Last digit of {number} is -{last_digit} and is greater than 5")
-    else:
-        print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
-    if number < 0:
-        print(f"Last digit of {number} is -{last_digit} and is 0")
-    else:
-        print(f"Last digit of {number} is {last_digit} and is 0")
+    print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    if number < 0:
-        print(f"Last digit of {number} is -{last_digit} and is less than 6 and not 0")
-    else:
-        print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
