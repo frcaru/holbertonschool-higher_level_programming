2ac79bd6bae5a3d41dc59ef945b65a2e26143ceb
Marine <5641@holbertonstudents.com>
2023-01-25 13:50:17 +0100

    format correction

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -9,6 +9,6 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if (i < len(matrix)) and (j < (len(matrix[0]) - 1)):
-                    print("{:d}".format(matrix[i][j]), end='')
+                    print("{:d} ".format(matrix[i][j]), end='')
                 else:
                     print("{:d}".format(matrix[i][j]))
