c58835e7e673ed71db32b7fd6a544cb954f85519
Marine <5641@holbertonstudents.com>
2023-01-25 13:45:56 +0100

    task 11: 2nd try

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -6,10 +6,8 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
 
-    new_list = []
     for i in my_list:
-        if my_list[my_list.index(i)] != my_list[idx]:
-            new_list.append(my_list[my_list.index(i)])
+        if i == my_list[idx]:
+            my_list.remove(i)
 
-    my_list = new_list
     return my_list
