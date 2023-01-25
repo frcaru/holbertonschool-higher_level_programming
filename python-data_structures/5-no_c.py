afb8c3b6ed9fdc74e635e708866a89c1793cdc04
Marine <5641@holbertonstudents.com>
2023-01-25 13:20:18 +0100

    2nd try

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,5 +3,11 @@
 
 def no_c(my_string):
 
-    copy = my_string.replace('c', '').replace('C', '')
+    copy = ""
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            copy += ''
+        else:
+            copy += i
+
     return copy
