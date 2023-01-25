8931eabd6247fce5bf2ccdcf75e44cdb546f0bef
Marine <5641@holbertonstudents.com>
2023-01-25 13:29:20 +0100

    if not sentence correction

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,9 @@
 
 def multiple_returns(sentence):
 
-    if not sentence:
-        sentence.append(None)
-    new_tuple = (len(sentence), sentence[0])
+    print(len(sentence))
+    if len(sentence) < 1:
+        new_tuple = (len(sentence), None)
+    else:
+        new_tuple = (len(sentence), sentence[0])
     return new_tuple
