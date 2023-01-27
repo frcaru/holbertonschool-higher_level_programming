51e756db8de937944ecf44c903ae80cad2a35b7a
Marine <5641@holbertonstudents.com>
2023-01-27 15:41:26 +0100

    12 Task: 10th attempt

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,64 +3,33 @@
 
 def roman_to_int(roman_string):
 
-    if not roman_string or (type(roman_string) != str):
-        return None
-    result = 0
+#M = 1000
+#D = 500
+#C = 100
+#L = 50
+#X = 10
+#V = 5
+#I = 1
 
-    for r in roman_string:
-        if r == 'M':
-            if roman_string[roman_string.index(r)] == roman_string[0]:
-                reuslt += 1000
-            elif roman_string[roman_string.index(r) - 1] == 'C':
-                result += 900
-            else:
-                result += 1000
+    roman_dict = {
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1,
+    }
 
-        if r == 'D':
-            if roman_string[roman_string.index(r)] == roman_string[0]:
-                result += 500
-            elif roman_string[roman_string.index(r) - 1] == 'C':
-                result += 400
-            else:
-                result += 500
+    if not isinstance(roman_string, str) or roman_string == None:
+        return 0
 
-        if r == 'C':
-            if roman_string[roman_string.index(r)] == roman_string[0]:
-                result += 100
-            elif roman_string[roman_string.index(r) - 1] == 'X':
-                result += 90
-            else:
-                result += 100
+    summ = 0
 
-        if r == 'L':
-            if roman_string[roman_string.index(r)] == roman_string[0]:
-                result += 50
-            elif roman_string[roman_string.index(r) - 1] == 'X':
-                result += 40
-            else:
-                result += 50
-
-        if r == 'X':
-            if roman_string[roman_string.index(r)] == roman_string[0]:
-                result += 10
-            elif roman_string[roman_string.index(r) - 1] == 'I':
-                result += 9
-            else:
-                result += 10
-
-        if r == 'V':
-            if roman_string[roman_string.index(r)] == roman_string[0]:
-                result += 5
-            elif roman_string[roman_string.index(r) - 1] == 'I':
-                result += 4
-            else:
-                result += 5
-
-        if r == 'I':
-            if roman_string[roman_string.index(r)] == roman_string[0]:
-                result += 1
-            elif roman_string.index(r) == len(roman_string) - 1:
-                result += 1
-            elif roman_string[roman_string.index(r) + 1] == 'I':
-                result += 1
-    return result
+    for i in range(len(roman_string) - 1, -1, -1):
+        num = roman_dict[roman_string[i]]
+        if 3 * num < summ:
+            summ -= num
+        else:
+            summ += num
+    return summ
