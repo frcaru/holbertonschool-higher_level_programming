2c8986aba9928cc2e3dd8de863db219552d77e47
Marine <5641@holbertonstudents.com>
2023-01-31 18:40:59 +0100

    Task 1: 1st attempt

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Creation of a class named Square with a private attribute """
+
+
+class Square:
+    """ Class Square
+
+    Private attribute:
+    __size : size of the square """
+
+    def __init__(self, __size=0):
+        self.__size = __size
