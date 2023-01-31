d105ab3588cc3dd2b87fdb51dd3ac12ede01c511
Marine <5641@holbertonstudents.com>
2023-01-31 19:45:04 +0100

    Task 4: 1st attempt

---
+++ python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" Creation of class Square defined by its size """
+
+
+class Square:
+    """ Class Square
+
+    Attribute:
+    __size : the size of the square
+
+    Method:
+    area() : returns the square area
+
+    """
+
+    def __init__(self, __size=0):
+        """ Constructor method """
+
+        if isinstance(__size, int) is not True:
+            raise TypeError("size must be an integer")
+        if __size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = __size
+
+    def area(self):
+        """ Method that returns the current square area """
+
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        """ Getter method """
+        return self.__size
+
+    @size.setter
+    def size(self, value): 
+        """ Setter method """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
