9dd7020f2a2dd7f115dc9b0b02b68ba6017665c2
Marine <5641@holbertonstudents.com>
2023-01-31 18:57:16 +0100

    Task 2: 1st attempt

---
+++ python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Creation of a class named Square that's defined by
+private attribute __size """
+
+
+class Square:
+    """ Class Square
+
+    Private attribute:
+    __size : the size of the square
+    """
+    
+    def __init__(self, __size=0):
+
+        if isinstance(__size, int) is not True:
+            raise TypeError("size must be an integer")
+
+        if __size < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = __size
